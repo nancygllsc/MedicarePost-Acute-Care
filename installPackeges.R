438b9d86734fd27d3e0db99b324fd68bd425c112
nancygllsc <npulido@clemson.edu>
2024-09-27 19:27:53 -0400

    commit27

--- installPackeges.R
+++
@@ -1,7 +0,0 @@
-install.packages("readxl","caret","kernlab",
-                 "dplyr","ggplot2","knitr","manipulate",
-                 "Hmisc","pastecs","MASS","broom",
-                 "tidyverse","AppliedPredictiveModeling")
-
-
-#install pack
